fb60078d8547cca480a54979ff2968156f90ea9a
wrx1721267632 <1721267632@qq.com>
2016-08-12 08:27:33 +0800

    test

---
+++ Unit_5/thread_move.cpp
@@ -0,0 +1,28 @@
+/*************************************************************************
+	> File Name: thread_move.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 2016年08月11日 星期四 10时59分26秒
+ ************************************************************************/
+
+#include<iostream>
+#include<thread>
+#include<unistd.h>
+using namespace std;
+
+void func()
+{
+    sleep(3);
+    cout << " this is func" << endl;
+}
+
+int main()
+{
+    std::thread t(func);
+    sleep(1);
+    std::thread t1(std::move(t));
+    // t.join();
+    t1.join();
+
+    return 0;
+}
